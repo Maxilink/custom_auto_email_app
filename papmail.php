8a8393d9ce7e051d04ca6003a3ceeadb47839bf0
Maxilink <94912204+Maxilink@users.noreply.github.com>
2023-11-26 14:46:34 +0100

    Update papmail.php

--- papmail.php
+++ papmail.php
@@ -1,41 +1,5 @@
 <?php
 
-/**
- * Plugin Name: weMail - Email Marketing Simplified With WordPress
- * Description: Send Beautiful Email Newsletters with WordPress
- * Plugin URI: https://wordpress.org/plugins/wemail/
- * Author: weDevs
- * Author URI: https://getwemail.io/?utm_source=wp-org&utm_medium=author-uri
- * Version: 1.14.2
- * License: GPL-3.0
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: wemail
- * Domain Path: /i18n/languages
- *
- * Copyright (c) 2020 weMail (email: info@getwemail.io). All rights reserved.
- *
- * Released under the GPL license
- * http://www.opensource.org/licenses/gpl-license.php
- *
- * This is an add-on for WordPress
- * http://wordpress.org/
- *
- * **************************************************************************
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- * **************************************************************************
- */
 
 // don't call the file directly
 if ( ! defined( 'ABSPATH' ) ) {
